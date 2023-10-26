276906d2744b6d99805287c943105a3fca42acd2
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2023-10-26 18:30:36 -0300

    Atualizar o install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-rm -f /usr/bin/dtmenu /usr/bin/proxy; curl -s -L -o /usr/bin/dtmenu https://github.com/InfinoteNet/Proxy-Tunel/raw/main/$(uname -i)/dtmenu
+rm -f /usr/bin/dtmenu /usr/bin/proxy; curl -s -L -o /usr/bin/dtmenu https://github.com/InfiniteNet/Proxy-Tunel/raw/main/$(uname -i)/dtmenu
 curl -s -L -o /usr/bin/proxy https://github.com/InfiniteNet/Proxy-Tunel/raw/main/$(uname -i)/proxy
 chmod +x /usr/bin/dtmenu /usr/bin/proxy
 clear && echo -e "\033[1;31mExecute: \033[1;32mdtmenu\033[0m"
